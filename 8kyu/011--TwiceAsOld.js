5564c5addbdb7cabde519a7b066dea09c31137cf
MSX <tn29172917@163.com>
2018-11-15 06:33:07 +0800

    Create 011--TwiceAsOld.js

---
+++ 8kyu/011--TwiceAsOld.js
@@ -0,0 +1,21 @@
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son
+ (or in how many years he will be twice as old).
+*/
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  var f = dadYearsOld;
+  var s = sonYearsOld;
+  var x = f - s * 2;
+  if (x < 0) {
+     x = x * (-1);
+  }
+  else {
+      x
+  }
+  return x;
+}
